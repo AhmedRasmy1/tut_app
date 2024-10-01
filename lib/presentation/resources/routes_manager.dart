0c97e5aa1adca1a5afb48193fa686934cfd84d26
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-01 22:23:43 +0300

    adding route manager done

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/forget_password/forget_password.dart';
+import 'package:tut_app/presentation/login/login_view.dart';
+import 'package:tut_app/presentation/main/main_view.dart';
+import 'package:tut_app/presentation/register/register_view.dart';
+import 'package:tut_app/presentation/splash/splash_view.dart';
+import 'package:tut_app/presentation/store_details/store_details_view.dart';
+
 class RoutesManager {
   static const String splashRoute = '/';
   static const String loginRoute = '/login';
@@ -5,4 +13,36 @@ class RoutesManager {
   static const String forgetPasswordRoute = '/forgetPassword';
   static const String mainRoute = '/main';
   static const String storedetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesManager.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case RoutesManager.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case RoutesManager.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case RoutesManager.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPassword());
+      case RoutesManager.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case RoutesManager.storedetailsRoute:
+        return MaterialPageRoute(
+            builder: (context) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text('No Route Found'),
+              ),
+              body: const Center(child: Text('No Route Found')),
+            ));
+  }
 }
