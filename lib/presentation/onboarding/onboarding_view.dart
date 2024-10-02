7ac304a8dbd427a848e5e4eca83bc879d8f3cf5a
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-02 12:12:53 +0300

    implementation onBoarding screen part 1

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tut_app/presentation/resources/assets_manager.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
@@ -8,8 +13,61 @@ class OnboardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnboardingView> {
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+  late final List<SliderObject> _sliders = getSliderObject();
+
+  List<SliderObject> getSliderObject() => [
+        SliderObject(
+          title: AppStrings.onBoardingTitle1,
+          subTitle: AppStrings.onBoardingSubTitle1,
+          image: ImageAssets.onBoardingLogo1,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle2,
+          subTitle: AppStrings.onBoardingSubTitle2,
+          image: ImageAssets.onBoardingLogo2,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle3,
+          subTitle: AppStrings.onBoardingSubTitle3,
+          image: ImageAssets.onBoardingLogo3,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle4,
+          subTitle: AppStrings.onBoardingSubTitle4,
+          image: ImageAssets.onBoardingLogo4,
+        ),
+      ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: _sliders.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {}),
+    );
   }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+  SliderObject({
+    required this.title,
+    required this.subTitle,
+    required this.image,
+  });
 }
